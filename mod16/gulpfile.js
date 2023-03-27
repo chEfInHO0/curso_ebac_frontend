eb354d9a4a46ac9c97c9f8954ec82a5a7dba62c7
Luccas <luccaselias0@gmail.com>
2023-03-26 22:45:38 -0300

    Separando arquivos em mais pastas para melhor organização

---
+++ mod16/gulpfile.js
@@ -0,0 +1,33 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const sourcemaps = require('gulp-sourcemaps')
+
+function compileSass(){
+    return gulp.src('./source/styles/main.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle:'compressed'}))
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('./build/styles'))
+}
+
+function primeiraFunc(callback){
+    console.log('Executando via GULP')
+    callback()
+}
+
+function dizOi(callback){
+    console.log('Oi GULP')
+    tchauGulp()
+    callback()
+}
+
+function tchauGulp(){
+    console.log('Tchau GULP')
+}
+
+exports.default = gulp.series(primeiraFunc,dizOi) // gulp.parallel(primeiraFunc,dizOi)
+exports.oi = dizOi
+exports.sass = compileSass
+exports.watch = function () {
+    gulp.watch('./source/styles/*scss',{ignoreInitial:false},gulp.series(compileSass))
+}
